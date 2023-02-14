62a75041cf34c417547de66b2d87d3d6327529b5
Harsheet Chordiya <76674531+Harsheet196@users.noreply.github.com>
2023-02-14 10:28:49 +0530

    resume link updated

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -34,7 +34,7 @@ const Services = () => {
           <br />
          Whatever it takes!
         </span>
-        <a href="https://drive.google.com/file/d/1-OrtivcBRoLjvpkTU7aObc7NQ3xYBwr7/view?usp=sharing">
+        <a href="https://drive.google.com/file/d/1RIBJDkomlNvxbyOaa4xFuNCnk6U3rN-b/view?usp=sharing">
           <button className="button s-button">Download CV</button>
         </a>
         <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
