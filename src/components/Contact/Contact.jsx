59c0468c8c94637f002b4ebec8a688fdf2b770ab
Harsheet Chordiya <76674531+Harsheet196@users.noreply.github.com>
2023-02-14 10:28:00 +0530

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -38,7 +38,7 @@ const Contact = () => {
           {/* darkMode */}
           <span style={{color: darkMode?'white': ''}}>Get in Touch</span>
           <span>Contact me</span>
-          <a href="https://drive.google.com/file/d/1-OrtivcBRoLjvpkTU7aObc7NQ3xYBwr7/view?usp=sharing">
+          <a href="https://drive.google.com/file/d/1RIBJDkomlNvxbyOaa4xFuNCnk6U3rN-b/view?usp=sharing">
           <button className="button s-button">Download CV</button>
           </a>
           <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}>
